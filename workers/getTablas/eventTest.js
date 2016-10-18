4367f593941859a3e7792fc57635a63898ca79ca
jozemario <josezero1@gmail.com>
2016-10-18 04:54:00 -0600

    Con DB Config

--- workers/getTablas/eventTest.js
+++ workers/getTablas/eventTest.js
@@ -2,13 +2,15 @@
 process.stdin.resume();
 // Listen for incoming data:
 var sql = require("seriate");
-var configDb = require('../../server/configDb.js');
+var configDb = require('./../../server/configDb.js');
+console.log('configDb', configDb);
 sql.setDefaultConfig( configDb );
 
 process.on('message', function (m) {
-
-  switch(m){
-    case 'getTablas':{
+  console.log(m);
+  console.log(m.hello);
+  switch(m.hello){
+    case "world":{
       sql.execute( {
         query: "SELECT * FROM INFORMATION_SCHEMA.TABLES"
       } ).then( function( results ) {
@@ -17,9 +19,13 @@ process.on('message', function (m) {
         console.log(`This process is pid ${process.pid}`);
         process.send({ foo: results });
         process.kill(process.pid);
-      }, function( err ) {
+        }, function( err ) {
         console.log( "Something bad happened:", err );
-      } );
+        console.log(`This process is pid ${process.pid}`);
+        process.send({ foo: err });
+        process.kill(process.pid);
+        } );
+      break;
     }
     default:{
       console.log('CHILD got message:', m);
