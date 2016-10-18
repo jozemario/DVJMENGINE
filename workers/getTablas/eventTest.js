574257130767e07d5d7768dae91797930ee4be89
jozemario <josezero1@gmail.com>
2016-10-18 04:33:25 -0600

    Sql agregado

--- workers/getTablas/eventTest.js
+++ workers/getTablas/eventTest.js
@@ -6,10 +6,31 @@ var configDb = require('../../server/configDb.js');
 sql.setDefaultConfig( configDb );
 
 process.on('message', function (m) {
-  console.log('CHILD got message:', m);
-  console.log(`This process is pid ${process.pid}`);
-  process.send({ foo: 'bar' });
-  process.kill(process.pid);
+
+  switch(m){
+    case 'getTablas':{
+      sql.execute( {
+        query: "SELECT * FROM INFORMATION_SCHEMA.TABLES"
+      } ).then( function( results ) {
+        console.log( results );
+        console.log('CHILD got message:', m);
+        console.log(`This process is pid ${process.pid}`);
+        process.send({ foo: results });
+        process.kill(process.pid);
+      }, function( err ) {
+        console.log( "Something bad happened:", err );
+      } );
+    }
+    default:{
+      console.log('CHILD got message:', m);
+      console.log(`This process is pid ${process.pid}`);
+      process.send({ foo: 'bar' });
+      process.kill(process.pid);
+    }
+  }
+
+
+
 });
 
 
